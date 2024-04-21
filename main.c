1383f85a038c78a3f36d5fd0b046afe67cb4f007
algindanil <algindanil.st@gmail.com>
2024-04-21 19:41:55 +0300

    Fix

--- main.c
+++ main.c
@@ -1,64 +1,72 @@
-#include <sys/msg.h>
-#include <sys/ipc.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-struct msgbuf {
-    long mtype;
-    int mvalue;
-};
-
-void send_result(int msgid, int result, int result_type) {
-    struct msgbuf msg;
-    msg.mtype = result_type;
-    msg.mvalue = result;
-    msgsnd(msgid, &msg, sizeof(msg.mvalue), 0);
-}
+#include <unistd.h>
+#include <sys/types.h>
 
 int f(int x) {
     return x * x;
 }
 
 int g(int x) {
-    return x + 1; 
+    return x + 1;
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <integer value>\n", argv[0]);
-        return -1;
+
+int main() {
+    int x;
+    printf("Enter a value for x: ");
+    scanf("%d", &x);
+
+    int pipefd[2];
+    if (pipe(pipefd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
     }
 
-    int x = atoi(argv[1]);
-    int msgid = msgget(IPC_PRIVATE, 0666 | IPC_CREAT);
+    pid_t pid_f = fork();
 
-    if (fork() == 0) {
-        int result = f(x);
-        send_result(msgid, result, 1);
-        exit(0);
+    if (pid_f == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
     }
 
-    if (fork() == 0) {
-        int result = g(x);
-        send_result(msgid, result, 2);
-        exit(0);
-    }
+    if (pid_f == 0) { 
+        close(pipefd[0]);
 
-    struct msgbuf msg;
-    int results[2] = {0};
-    int count = 0;
-    while (count < 2) {
-        if (msgrcv(msgid, &msg, sizeof(msg.mvalue), 0, 0) > 0) {
-            results[msg.mtype - 1] = msg.mvalue;
-            count++;
+        dup2(pipefd[1], STDOUT_FILENO);
+        close(pipefd[1]);
+
+        int result_f = f(x);
+        printf("%d", result_f);
+        exit(EXIT_SUCCESS);
+    } else { 
+        pid_t pid_g = fork();
+
+        if (pid_g == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+
+        if (pid_g == 0) { 
+            close(pipefd[1]);
+
+            dup2(pipefd[0], STDIN_FILENO);
+            close(pipefd[0]);
+
+            int result_g;
+            scanf("%d", &result_g);
+
+            int result = result_g * g(x);
+            printf("%d", result);
+            exit(EXIT_SUCCESS);
+        } else { 
+            close(pipefd[0]);
+            close(pipefd[1]);
+
+            // wait(NULL);
+            // wait(NULL);
         }
     }
-
-    int fin_result = results[0] * results[1];
-    printf("Result: %d\n", fin_result);
-
-    msgctl(msgid, IPC_RMID, NULL);
 
     return 0;
 }
